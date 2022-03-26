42a34a9eb37f67363c0932cbb37c362ab982c86b
Ostap Melnychuk <ostapss222@gmail.com>
2022-03-26 22:42:25 +0200

    HttpServletRequest is now an input parameter

--- src/main/java/com/example/webhook/controller/WebhookController.java
+++ src/main/java/com/example/webhook/controller/WebhookController.java
@@ -4,6 +4,9 @@ import com.example.webhook.email.EmailSender;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.Map;
 
 @RestController
@@ -16,9 +19,26 @@ public class WebhookController {
         this.emailSender = emailSender;
     }
 
-    @PostMapping(consumes = "application/json")
-    public ResponseEntity<Void> receiveFigmaWebhook(@RequestBody Map<String, Object> payload) {
-        notifyAllDevelopers(payload.toString());
+//    @PostMapping(consumes = "application/json")
+//    public ResponseEntity<Void> receiveFigmaWebhook(@RequestBody Map<String, Object> payload) {
+//        notifyAllDevelopers(payload.toString());
+//        return ResponseEntity.ok().build();
+//    }
+
+    @PostMapping
+    public ResponseEntity<Void> receiveFigmaWebhook(HttpServletRequest request) {
+        StringBuilder result = new StringBuilder();
+        try {
+            BufferedReader reader = request.getReader();
+            String buf;
+            while((buf = reader.readLine()) != null) {
+                result.append(buf);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(result);
+        notifyAllDevelopers(result.toString());
         return ResponseEntity.ok().build();
     }
 
